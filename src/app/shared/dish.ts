03479fe01203915115f1877f75bc0fa000fcb930
Piero <grilli.piero@circeco.org>
2022-04-11 17:21:48 +0000

    data binding

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,5 @@
+import { Comment } from './comment';
+
 export class Dish {
     'id': string;
     'name': string;
@@ -7,8 +9,5 @@ export class Dish {
     'label': string;
     'price': string;
     'description': string;
-}
-
-export class DetailsDish extends Dish {
-    'comments': object[];
+    'comments': Comment[];
 }
